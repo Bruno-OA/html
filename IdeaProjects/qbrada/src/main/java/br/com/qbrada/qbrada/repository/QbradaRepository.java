d9107d92795d32c173c66b13bac41015aa34b15e
Bruno Oliveira <brunooliveiraalves78@gmail.com>
2020-02-04 13:23:30 -0300

    subir projeto completo

---
+++ IdeaProjects/qbrada/src/main/java/br/com/qbrada/qbrada/repository/QbradaRepository.java
@@ -0,0 +1,8 @@
+package br.com.qbrada.qbrada.repository;
+
+import br.com.qbrada.qbrada.model.Qbrada;
+import org.springframework.data.repository.CrudRepository;
+
+public interface QbradaRepository extends CrudRepository<Qbrada, String> {
+
+}
